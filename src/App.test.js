cc25d3bf0e55e5e7582aacf1863dab76ff50a995
Benjamin Chodoroff (bnchdrff) <anonyben@gmail.com>
2017-09-24 16:30:53 -0400

    add semis

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import App from './App'
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
 
 it('renders', () => {
-  const wrapper = shallow(<App />)
-})
+  const wrapper = shallow(<App />);
+});

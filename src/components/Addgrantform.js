2b3910fc5355432fe0dc2e96336cc43e0f88db4b
jessica <jessica.mcinchak@gmail.com>
2018-03-26 22:33:29 -0400

    classy material ui form inputs

--- src/components/Addgrantform.js
+++ src/components/Addgrantform.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Field, reduxForm } from 'redux-form';
+import { reduxForm } from 'redux-form';
+import TextField from 'material-ui/TextField';
+import { InputAdornment } from 'material-ui/Input';
+import Button from 'material-ui/Button';
 
 import * as actions from '../actions';
 import { withPdfs } from '../containers/WithPdfs';
@@ -14,12 +17,13 @@ class Addgrantform extends Component {
 
     this.state = {
       recipient: '',
-      amount: 0,
+      amount: null,
       description: '',
-      startDate: this.props.pdf.year,
-      endDate: this.props.pdf.year,
+      startDate: `01/${this.props.pdf.year}`,
+      endDate: `12/${this.props.pdf.year}`,
       funder: this.props.pdf.org.name,
       source: this.props.source,
+      internalNotes: '',
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
@@ -28,8 +32,8 @@ class Addgrantform extends Component {
 
   createGrant(grant) {
     this.props.createGrant({
-      start: `01/${this.state.startDate}`,
-      end: `12/${this.state.endDate}`,
+      start: this.state.startDate,
+      end: this.state.endDate,
       funder: { id: this.props.pdf.org.id },
       recipient: { id: 123 },
       source: 'hi',
@@ -41,54 +45,90 @@ class Addgrantform extends Component {
     });
   }
 
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-
+  handleInputChange = name => event => {
     this.setState({
-      [name]: value
+      [name]: event.target.value,
     });
-  }
+  };
 
   handleSubmit(event) {
     console.log(this.state);
-    this.createGrant(this.state);
-    event.preventDefault();
+    // this.createGrant(this.state);
+    // event.preventDefault();
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <div>
-          <label htmlFor="startDate">Start Date</label>
-          <Field name="startDate" component="input" type="text" onChange={this.handleInputChange} />
+        <div style={{ display: 'flex', flexWrap: 'wrap', }}>
+          <TextField
+            id="startDate"
+            label="Start date"
+            value={this.state.startDate}
+            onChange={this.handleInputChange('startDate')}
+            margin="normal"
+            required />
+          <TextField
+            id="endDate"
+            label="End date"
+            value={this.state.endDate}
+            onChange={this.handleInputChange('endDate')}
+            margin="normal"
+            required />
         </div>
-        <div>
-          <label htmlFor="endDate">End Date</label>
-          <Field name="endDate" component="input" type="text" onChange={this.handleInputChange} />
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <TextField
+            id="funder"
+            label="From"
+            value={this.state.funder}
+            onChange={this.handleInputChange('funder')}
+            margin="normal"
+            required />
+          <TextField
+            id="recipient"
+            label="To"
+            value={this.state.recipient}
+            onChange={this.handleInputChange('recipient')}
+            helperText="Can't find the recipient org? Add them here"
+            margin="normal"
+            required />
+          <TextField
+            id="amount"
+            label="Amount"
+            value={this.state.amount}
+            onChange={this.handleInputChange('amount')}
+            InputProps={{
+              startAdornment: <InputAdornment position="start">$</InputAdornment>,
+            }}
+            margin="normal"
+            required />
+          <TextField
+            id="description"
+            label="Purpose or description"
+            value={this.state.description}
+            onChange={this.handleInputChange('description')}
+            multiline
+            margin="normal" />
+          <TextField
+            id="internalNotes"
+            label="Internal notes"
+            value={this.state.description}
+            onChange={this.handleInputChange('internalNotes')}
+            multiline
+            margin="normal" />
+          <TextField
+            id="source"
+            label="Source"
+            value={this.state.source}
+            onChange={this.handleInputChange('source')}
+            margin="normal"
+            required />
         </div>
-        <div>
-          <label htmlFor="funder">Funder</label>
-          <Field name="funder" component="input" type="text" onChange={this.handleInputChange} />
+        <div style={{ display: 'flex', justifyContent: 'center', margin: '1em', }}>
+          <Button type="submit" variant="raised" color="primary" size="large">
+            Submit
+          </Button>
         </div>
-        <div>
-          <label htmlFor="recipient">Recipient</label>
-          <Field name="recipient" component="input" type="text" onChange={this.handleInputChange} />
-        </div>
-        <div>
-          <label htmlFor="amount">Amount</label>
-          <Field name="amount" component="input" type="text" onChange={this.handleInputChange} />
-        </div>
-        <div>
-          <label htmlFor="description">Description/Purpose</label>
-          <Field name="description" component="textarea" type="text" onChange={this.handleInputChange} />
-        </div>
-        <div>
-          <label htmlFor="source">Source</label>
-          <Field name="source" component="input" type="text" onChange={this.handleInputChange} />
-        </div>
-        <button type="submit">Submit</button>
       </form>
     );
   }

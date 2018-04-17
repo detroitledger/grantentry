81b9823b179e6c16876b56a11ec3a92ad751b88a
jessica <jessica.mcinchak@gmail.com>
2018-04-16 20:45:09 -0400

    another go at field level validation

--- src/components/Addgrantform.js
+++ src/components/Addgrantform.js
@@ -11,25 +11,46 @@ import { withPdfs } from '../containers/WithPdfs';
 
 import './Addgrantform.css';
 
+/** Group of functions that take TextField input and return a bool */
+const validators = {
+  isNotNumber: function(val) {
+    return isNaN(Number(val))
+  },
+
+  isFormattedAsDate: function(val) {
+    let mmyyyy = RegExp('(0[1-9]|10|11|12)/20[0-9]{2}$')
+    return mmyyyy.test(val);
+  },
+
+  isAfterDate: function(val) {
+    // check end date is after start date
+  },
+
+  isValidLength: function(val) {
+    // check that source string is reasonable length
+  }
+};
+
 class Addgrantform extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      recipient: '',
-      amount: undefined,
-      description: '',
       startDate: `01/${this.props.pdf.year}`,
       endDate: `12/${this.props.pdf.year}`,
       funder: this.props.pdf.org.name,
-      source: this.props.source,
+      recipient: '',
+      amount: 0,
+      description: '',
       internalNotes: '',
-      error: false,
-      helperText: '',
+      source: this.props.source,
+      errors: {},
+      helpers: {
+        recipient: "Can't find the recipient org? Add them here",
+      },
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleAmountChange = this.handleAmountChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -49,20 +70,21 @@ class Addgrantform extends Component {
   }
 
   handleInputChange = name => event => {
-    this.setState({
-      [name]: event.target.value,
-    });
-  }
+    this.setState({ [name]: event.target.value, });
 
-  handleAmountChange = name => event => {
-    if (event.target.value && isNaN(Number(event.target.value))) {
-      this.setState({ error: true, helperText: 'Must be a number', });
-    } else {
-      this.setState({ 
-        [name]: event.target.value,
-        error: false,
-        helperText: '',
-      });
+    // Field level validation to trigger error messages
+    if (name === 'amount') {
+      validators.isNotNumber(event.target.value) ? 
+        this.setState({ errors: { [name]: true }, helpers: { [name]: 'Must be a positive number' }, }) 
+        : this.setState({ errors: { [name]: false }, helpers: { [name]: '' }, });
+    } else if (name === 'startDate' || name === 'endDate') {
+      validators.isFormattedAsDate(event.target.value) ?
+        this.setState({ errors: { [name]: false }, helpers: { [name]: '' }, })
+        : this.setState({ errors: { [name]: true }, helpers: { [name]: 'Format like MM/YYYY' }, });
+    } else if (name === 'endDate') {
+      // should come after startDate
+    } else if (name === 'source') {
+      // is valid length 
     }
   }
 
@@ -82,6 +104,8 @@ class Addgrantform extends Component {
             value={this.state.startDate}
             onChange={this.handleInputChange('startDate')}
             margin="normal"
+            helperText={this.state.helpers.startDate}
+            error={this.state.errors.startDate}
             required />
           <TextField
             id="endDate"
@@ -89,6 +113,8 @@ class Addgrantform extends Component {
             value={this.state.endDate}
             onChange={this.handleInputChange('endDate')}
             margin="normal"
+            helperText={this.state.helpers.endDate}
+            error={this.state.errors.endDate}
             required />
         </div>
         <div style={{ display: 'flex', flexDirection: 'column' }}>
@@ -98,26 +124,29 @@ class Addgrantform extends Component {
             value={this.state.funder}
             onChange={this.handleInputChange('funder')}
             margin="normal"
+            helperText={this.state.helpers.funder}
+            error={this.state.errors.funder}
             required />
           <TextField
             id="recipient"
             label="To"
             value={this.state.recipient}
             onChange={this.handleInputChange('recipient')}
-            helperText="Can't find the recipient org? Add them here"
+            helperText={this.state.helpers.recipient}
+            error={this.state.errors.recipient}
             margin="normal"
             required />
           <TextField
             id="amount"
             label="Amount"
             value={this.state.amount}
-            onChange={this.handleAmountChange('amount')}
-            error={this.state.error}
-            helperText={this.state.helperText}
+            onChange={this.handleInputChange('amount')}
+            margin="normal"
             InputProps={{
               startAdornment: <InputAdornment position="start">$</InputAdornment>,
             }}
-            margin="normal"
+            helperText={this.state.helpers.amount}
+            error={this.state.errors.amount}
             required />
           <TextField
             id="description"
@@ -139,6 +168,8 @@ class Addgrantform extends Component {
             value={this.state.source}
             onChange={this.handleInputChange('source')}
             margin="normal"
+            helperText={this.state.helpers.source}
+            error={this.state.errors.source}
             required />
         </div>
         <div style={{ display: 'flex', justifyContent: 'center', margin: '1em', }}>
